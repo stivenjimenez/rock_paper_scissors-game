706d4ed880af5c2a0135db2ee978c8f6c3b2962e
stiven jimenez <sajqprogramming@gmail.com>
2022-05-17 07:32:50 -0500

    🎉 init project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        width: "100vw",
+        height: "100vh",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1> 🎉 Hello word</h1>
+    </div>
+  );
+}
+
+export default App;
